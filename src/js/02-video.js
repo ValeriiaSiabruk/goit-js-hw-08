83abc427e5af6c2305d92e6045e59e8eb4092d41
Valeriia Siabruk <vsyabryk@gmail.com>
2022-07-07 16:59:49 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+console.log(throttle);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const setCurrentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', data => {
+  setCurrentTime(data);
+});
+
+player.setCurrentTime(+localStorage.getItem('videoplayer-current-time'));
